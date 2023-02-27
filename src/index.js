00681338b6b4e829db0d531ce87e121631968f95
RisUma1803 <umidjonergashev1805@gmail.com>
2023-02-27 23:08:16 +0500

    human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function toReadable (number) {
+  const ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    
+    const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
   
+    let result = "";
+  
+    if (number === 0) {
+      return ones[number];
+    }
+  
+    if (number >= 100) {
+      result += ones[Math.floor(number / 100)] + " hundred ";
+      number %= 100;
+    }
+  
+    if (number >= 20) {
+      result += tens[Math.floor(number / 10)] + " ";
+      number %= 10;
+    }
+  
+    if (number > 0) {
+      result += ones[number];
+    }
+  
+    return result.trim();
 }
